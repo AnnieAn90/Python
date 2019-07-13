bc28cea9b1350098622437927a70e7fba47d94c9
ZipengHuang <32931890+ZipengHuang@users.noreply.github.com>
2019-07-13 18:45:51 -0300

    ZP

--- Python/Python MIT Course/Script/fixed_monthly_payment_bisection.py
+++ Python/Python MIT Course/Script/fixed_monthly_payment_bisection.py
@@ -4,11 +4,11 @@ THIS CODE COMPUTES THE FIXED MONTHLY PAYMENT THAT REQUIRED TO PAYOFF THE BALANCE
 USING BYSECTION SEARCH
 """
 
-def remainingBalance(balance,fixedPayment,annualInterestRate):
+def remainingBalance(balance,fixedPayment,annualInterestRatem,month):
     """
     Calculate the annual balance after a fixed monthly payment and anuual interest rate
     """
-    for i in range(0,12):
+    for i in range(0,month):
        unpaidBalance = balance - fixedPayment
        if unpaidBalance >0:
            interest = unpaidBalance*annualInterestRate/12
@@ -17,25 +17,29 @@ def remainingBalance(balance,fixedPayment,annualInterestRate):
        balance = unpaidBalance+interest
     return balance
 
-balance = 999999 # money you owe bank
-annualInterestRate = 0.18 
+
+amortization = 25 # 25 years
+month = amortization*12 # payment month
+balance = 329900 # money you owe bank
+annualInterestRate = 2.79
 monthlyRate = annualInterestRate/12 
 
 # set lo/up bounds
-lobound = balance/12
-upbound = (balance*(1+monthlyRate)**12)/12
+lobound = balance/month
+upbound = (balance*(1+monthlyRate)**month)/month
 
 # start bisection search
 mid = (lobound + upbound)/2
-yearBalance = remainingBalance(balance,mid,annualInterestRate)
+New_balance = remainingBalance(balance,mid,annualInterestRate,month)
 
-while abs(yearBalance)> 0.01:
-    if yearBalance > 0:
+while abs(New_balance)> 1000:
+    if New_balance > 0:
         lobound = mid
     else:
         upbound = mid
     mid = (lobound + upbound)/2
-    yearBalance = remainingBalance(balance,mid,annualInterestRate)
+    New_balance = remainingBalance(balance,mid,annualInterestRate,month)
+    print("Code is stilling running! ")
 
 mid = round(mid,2)
 
