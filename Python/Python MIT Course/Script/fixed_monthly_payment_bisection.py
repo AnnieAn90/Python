bac5e1a61064d147269ae30c3041089ccb73c5ac
ZipengHuang <32931890+ZipengHuang@users.noreply.github.com>
2019-07-14 00:09:13 -0300

    ZP

--- Python/Python MIT Course/Script/fixed_monthly_payment_bisection.py
+++ Python/Python MIT Course/Script/fixed_monthly_payment_bisection.py
@@ -17,11 +17,20 @@ def remainingBalance(balance,fixedPayment,annualInterestRatem,month):
        balance = unpaidBalance+interest
     return balance
 
+def MoneyToday(payment,period,monthInterestRate):
+    if period==0:
+        return payment
+    else:
+        return payment/pow(1+monthInterestRate,period)
+    
 
 amortization = 25 # 25 years
 month = amortization*12 # payment month
-balance = 329900 # money you owe bank
-annualInterestRate = 2.79
+price = 329900 # money you owe bank
+downPayment = 30/100*price # downpayment percentage
+balance = price-downPayment
+
+annualInterestRate = 5/100
 monthlyRate = annualInterestRate/12 
 
 # set lo/up bounds
@@ -32,20 +41,28 @@ upbound = (balance*(1+monthlyRate)**month)/month
 mid = (lobound + upbound)/2
 New_balance = remainingBalance(balance,mid,annualInterestRate,month)
 
-while abs(New_balance)> 1000:
+while abs(New_balance)> 0.01:
     if New_balance > 0:
         lobound = mid
     else:
         upbound = mid
     mid = (lobound + upbound)/2
     New_balance = remainingBalance(balance,mid,annualInterestRate,month)
-    print("Code is stilling running! ")
+    print("Balance is ",New_balance)
 
 mid = round(mid,2)
 
-print("Lowest Payment: "+ str(mid))
+print("Monthly Payment: "+ str(mid))
 
+interestRate = 1.8/100 # annual saving interest rate
+monthInterestRate = interestRate/12 
 
+moneyToday = 0
+
+for i in range(0,month):
+    moneyToday = moneyToday + MoneyToday(mid,i,monthInterestRate)
+
+print("You need to spend ", moneyToday+downPayment, "at today's value!!!" )
 
       
       
