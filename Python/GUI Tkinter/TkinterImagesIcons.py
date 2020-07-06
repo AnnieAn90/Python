77f4abeadce2bbfe1ad722551afee8a9330c89bf
ZipengHuang <32931890+ZipengHuang@users.noreply.github.com>
2020-07-06 12:22:43 -0300

    z

--- Python/GUI Tkinter/TkinterImagesIcons.py
+++ Python/GUI Tkinter/TkinterImagesIcons.py
@@ -7,15 +7,19 @@ NOTE using this kind of import method we do not need the . sign
 or we can use "import tkinter". But with this we need to use tkiner.Tk().
 Another way is to use "import tkinter as gui", use gui.TK(), alising! 
 
-Include images and icons
+Include images and icons (i.e. feather)
 """
     
 # just a blank window，with min/max/close three buttons
 root =Tk() 
+root.title("Images and Icons")
 #root.geometry("500x300") # dimension of root window
 
-photo = PhotoImage(file="pic1.jpg")
-label = Label(root,image=photo)
-label.pack()
+# **** icon ****
+root.iconbitmap()
+
+#photo = PhotoImage(file="pic1.jpg")
+#label = Label(root,image=photo)
+#label.pack()
 
 root.mainloop()
